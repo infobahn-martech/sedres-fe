fac564c1d49ca1858ef4769dc2c56f66f3cba51d
Dany Thomas <prog3@infobahnworld.com>
2026-09-17 10:51:15 +0400

    updates

--- src/structure/Header/index.jsx
+++ src/structure/Header/index.jsx
@@ -96,9 +96,9 @@ function Header({ onMenuToggle, mobileMenuOpen: externalMobileMenuOpen, activePo
     { label: 'Stickers', modal: 'stickers' },
     { label: 'Tags', modal: 'tags' },
     { label: 'Types', modal: 'types' },
-    { label: 'Templates', modal: 'templates' },
-    { label: 'Templates List', modal: 'templates list' },
-    { label: 'Custom Templates', modal: 'custom templates' },
+    // { label: 'Templates', modal: 'templates' },
+    { label: 'Custom Templates', modal: 'templates list' },
+    // { label: 'Custom Templates', modal: 'custom templates' },
   ];
 
   const getLoggedInUser = () => {
